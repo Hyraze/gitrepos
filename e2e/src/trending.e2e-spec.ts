1e892cacdc5d9e50cb6e02d8bff205dc43730523
hanishrao <hanishrao321@gmail.com>
2023-09-11 12:44:34 +0530

    test: :sparkles: updated node version for test case

--- e2e/src/trending.e2e-spec.ts
+++ e2e/src/trending.e2e-spec.ts
@@ -1,6 +1,4 @@
 import { TrendingPage } from './trending.po';
-import { notEqual } from 'assert';
-
 describe('gitrepos trending view', () => {
   let page: TrendingPage;
 
@@ -8,12 +6,8 @@ describe('gitrepos trending view', () => {
     page = new TrendingPage();
   });
 
-
   it('should show a repo name', () => {
     page.navigateTo();
     expect(page.getRepoName().getText()).toBeDefined();
   });
-
-
-
 });
